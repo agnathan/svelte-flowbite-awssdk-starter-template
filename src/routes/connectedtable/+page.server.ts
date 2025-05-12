3344293bd869b132d05416b0749eba40c3b59b1c
Daniel Holmlund <agnathan@gmail.com>
2025-05-12 11:25:02 -0700

    get id_token from cookies

--- src/routes/connectedtable/+page.server.ts
+++ src/routes/connectedtable/+page.server.ts
@@ -1,7 +1,6 @@
 // Import SvelteKit types and libraries
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
-import { unmarshall } from '@aws-sdk/util-dynamodb';
 
 import { getAWSIdentityId } from '$lib/auth/cognito';
 import { getDynamoDBClient } from '$lib/db/awsClient';
@@ -9,8 +8,9 @@ import { getDataByUser } from '$lib/db/queries/getDataByUser';
 // src/routes/+page.server.ts
 
 
-export const load: PageServerLoad = async ({ locals }) => {
-  const idToken = locals.user?.id_token;
+export const load: PageServerLoad = async ({ locals, cookies }) => {
+
+  const idToken = cookies.get('id_token') || locals.user?.id_token;
   if (!idToken) throw error(401, 'Authentication required.');
 
   try {
