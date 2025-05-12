39ad857663e327471a02aaf0340fe1a553e0d251
Daniel Holmlund <agnathan@gmail.com>
2025-05-12 15:42:56 -0700

    AGAIN

--- src/routes/connectedtable/+page.server.ts
+++ src/routes/connectedtable/+page.server.ts
@@ -7,21 +7,38 @@ import { getDynamoDBClient } from '$lib/db/awsClient';
 import { getDataByUser } from '$lib/db/queries/getDataByUser';
 // src/routes/+page.server.ts
 
-
 export const load: PageServerLoad = async ({ locals, cookies }) => {
-  console.log("Entering Server side load function: /connectedtable");
-  console.log("locals: ", locals);
+	console.log('Entering Server side load function: /connectedtable');
+	console.log('locals: ', locals);
 
-  console.log("cookies: ", cookies.getAll());
-  const idToken = cookies.get('id_token') || locals.user?.id_token;
-  if (!idToken) throw error(401, 'Authentication required.');
+	console.log('cookies: ', cookies.getAll());
+	const idTokencookies = cookies.get('id_token');
+	const idTokenLocals = locals.user?.id_token;
+	const idToken = cookies.get('id_token') || locals.user?.id_token;
+	console.log('idToken: ', idToken);
+	console.log('idTokenLocals: ', idTokenLocals);
+	console.log('idTokencookies: ', idTokencookies);
+	if (!idToken) throw error(401, 'Authentication required. IdToken is not set.');
 
-  try {
-    const identityId = await getAWSIdentityId({ idToken });
-    const ddb        = await getDynamoDBClient(idToken);
-    const data       = await getDataByUser(ddb, identityId);
-    return { data };
-  } catch {
-    throw error(401, 'Authentication required.');
-  }
+	let identityId;
+	let ddb;
+	let data;
+	try {
+		identityId = await getAWSIdentityId({ idToken });
+	} catch {
+		throw error(401, 'Authentication required. getAWSIdentityId failed.');
+	}
+
+	try {
+		ddb = await getDynamoDBClient(idToken);
+	} catch {
+		throw error(401, 'Authentication required. getDynamoDBClient failed.');
+	}
+
+	try {
+		data = await getDataByUser(ddb, identityId);
+		return { data };
+	} catch {
+		throw error(401, 'Authentication required. getDataByUser failed.');
+	}
 };
